4ab3d09e363aaf51e763bdfe5f497c75b9d1ca70
shivam <shivamkrthakur18@gmail.com>
2024-02-25 18:16:17 +0530

    updates

--- production-bluebeads/src/constants.js
+++
@@ -1,1 +0,0 @@
-export const DB_NAME="blood_help"
--- production-bluebeads/src/index.js
+++
@@ -1,9 +0,0 @@
-import connectDB from "./db/index.js";
-import app from "./app.js";
-import dotenv from "dotenv"
-
-dotenv.config();
-
-connectDB()
-.then(()=>{ app.listen(process.env.PORT, ()=>{ console.log("started listening on : ",process.env.PORT);}) })
-.catch((error)=>{console.log("failed to connect database. Error is : ",error)})
